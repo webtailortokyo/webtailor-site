6da586d04a3f6c46c91f797f90b0ecf5e7854e0b
hiroko <webtailortokyo@gmail.com>
2026-04-01 22:31:28 +0900

    Fix canonical URL and update deploy workflow2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://webtailor.work',
+  integrations: [tailwind(), sitemap()],
   output: 'static'
 });
